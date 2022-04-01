92a7f91b1026c9b1c438220fd49dbd09d7475260
Roman Bykov <97618922+chingachguk1977@users.noreply.github.com>
2022-04-01 19:35:33 +0300

    everything works (D2)

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
+from datetime import datetime, date, time
 from django.contrib.auth.models import User
 from django.db.models import Sum
 
@@ -9,33 +10,39 @@ class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def update_rating(self):
-
         posts_rating = self.post_set.aggregate(post_rating_aggr=Sum('rating'))
         posts_rating_sum = 0
         posts_rating_sum += posts_rating.get('post_rating_aggr')
-        
+
         comments_rating = self.user.comment_set.aggregate(comments_rating_aggr=Sum('rating'))
         comments_rating_sum = 0
         comments_rating_sum += comments_rating.get('comments_rating_aggr')
-        
-        self.rating = posts_rating_sum*3 + comments_rating_sum
+
+        self.rating = posts_rating_sum * 3 + comments_rating_sum
+
+        # author_posts = self.post_set.all()
+        # for post in author_posts:
+        #     self.rating += post.rating * 3
+        #     post_comments = post.comment_set.all()
+        #     for comment in post_comments:
+        #         self.rating += comment.rating
+
         self.save()
 
         return self.rating
-    
+
     def __str__(self):
         return f'{self.user.username}, rating = {self.rating}'
 
 
 class Category(models.Model):
     cat_name = models.CharField(max_length=64, unique=True)
-    
+
     def __str__(self):
-        return self.cat_name    
+        return self.cat_name
 
 
 class Post(models.Model):
-
     ARTICLE = 'AR'
     NEWS = 'NW'
 
@@ -43,18 +50,21 @@ class Post(models.Model):
         (ARTICLE, 'article'),
         (NEWS, 'news')
     ]
-    
+
     type = models.CharField(max_length=2, choices=POST_CHOICES, default=NEWS)
     time_pub = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=128)
     body = models.TextField()
     rating = models.SmallIntegerField(default=0)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     cats = models.ManyToManyField(Category, through='PostCategory')
 
     def __str__(self):
-        return f"'{self.title}' by {self.author.user.username}, rating = {self.rating}"
-    
+        post_metadata = f"'{self.title}' by {self.author.user.username},\n \
+                          published on: {self.time_pub.strftime('%d/%m/%Y, %H:%M')},\n \
+                          the rating of this post is {self.rating}\nPreview: {self.preview()}"
+        return post_metadata
+
     def like(self):
         self.rating += 1
         self.save()
@@ -73,8 +83,8 @@ class Comment(models.Model):
     body = models.TextField()
     time_pub = models.DateTimeField(auto_now_add=True)
     rating = models.SmallIntegerField(default=0)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def like(self):
         self.rating += 1
@@ -84,11 +94,16 @@ class Comment(models.Model):
         self.rating -= 1
         self.save()
 
+    def __str__(self):
+        comment_metadata = f"{self.user.username} wrote: '{self.body[:64]}',\n \
+                             on: {self.time_pub.strftime('%d-%m-%Y, %H:%M')},\n \
+                             the rating of this comment is {self.rating}\n"
+        return comment_metadata
+
 
 class PostCategory(models.Model):
     post_thru = models.ForeignKey(Post, on_delete=models.CASCADE)
     cat_thru = models.ForeignKey(Category, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f'{self.post_thru} <-> {self.cat_thru}'
-    
