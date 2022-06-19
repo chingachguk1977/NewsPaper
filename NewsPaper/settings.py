c6221618252771ab3d18ef73f6648785874088fd
Roman Bykov <romabykov@gmail.com>
2022-06-19 12:30:50 +0300

    putting in some order

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -203,7 +203,8 @@ CELERY_RESULT_SERIALIZER = 'json'
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
+        # Указываем, куда будем сохранять кэшируемые файлы! Не забываем 
+        # создать папку cache_files внутри папки с manage.py!
         'LOCATION': os.path.join(BASE_DIR, 'cache_files'), 
     }
 }
