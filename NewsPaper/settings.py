f30c82d5fb01a7b133571dff666b0fc1bbb877b5
chingachguk1977 <romabykov@gmail.com>
2022-07-06 23:24:34 +0300

    D13, added logging

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -34,7 +34,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -117,7 +116,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -164,6 +162,8 @@ EMAIL_SUBJECT_PREFIX = '[Django Tutorial] --> '
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
@@ -177,7 +177,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -207,6 +206,236 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         # Указываем, куда будем сохранять кэшируемые файлы! Не забываем 
         # создать папку cache_files внутри папки с manage.py!
-        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), 
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'),
     }
 }
+
+BLUE = "\033[34m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+PURPLE = "\033[35m"
+ORIGIN_COLOR = "\033[0m"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': f'{ORIGIN_COLOR}%(asctime)s :{GREEN}: %(levelname)s :{ORIGIN_COLOR}: %(message)s'
+        },
+        'verbose': {
+            'format': f'{YELLOW}%(levelname)s %(asctime)s :{ORIGIN_COLOR}: \
+                       %(pathname)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'error_format': {
+            'format': f'{RED}%(levelname)s :{ORIGIN_COLOR}: %(asctime)s \
+                       %(pathname)s %(module)s %(process)d %(thread)d %(message)s %(exc_info)s'
+        },
+        'info_format': {
+            'format': f'{YELLOW}%(levelname)s :{ORIGIN_COLOR}: %(asctime)s %(module)s %(message)s'
+        },
+        'error_file_format': {
+            'format': '%(levelname)s %(asctime)s %(message)s %(pathname)s %(exc_info)s'
+        },
+        'security_format': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'mail_format': {
+            'format': '%(levelname)s %(asctime)s %(message)s %(pathname)s'
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'console_warning': {
+            'level': 'WARNING',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'console_error': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file_info': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'info_format',
+        },
+        'file_error': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'error.log',
+            'formatter': 'error_file_format',
+        },
+        'file_security': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'security.log',
+            'formatter': 'security_format',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'mail_format',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'console_warning', 'console_error', 'file_info'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['mail_admins', 'file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.template': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.db_backends': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['file_security'],
+            'level': 'INFO',
+            'propagate': False,
+        }
+    }
+}
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'standard': {
+#             'format': ORIGIN_COLOR + '{asctime} -- ' + GREEN + '{levelname}'
+#                       + ORIGIN_COLOR + ' -- {message}',
+#             'style': '{',
+#         },
+#         'forinfo': {
+#             'format': ORIGIN_COLOR + '{asctime} -- ' + BLUE + '{levelname}'
+#                       + ORIGIN_COLOR + ' -- {module} : {message}',
+#             'style': '{',
+#         },
+#         'forwarning': {
+#             'format': ORIGIN_COLOR + '{asctime} -- ' + YELLOW + '{levelname}'
+#                       + ORIGIN_COLOR + ' -- {pathname} : {message}',
+#             'style': '{',
+#         },
+#         'forerror': {
+#             'format': ORIGIN_COLOR + '{asctime} -- ' + PURPLE + '{levelname}'
+#                       + ORIGIN_COLOR + ' -- {pathname} / {exc_info} :{message}',
+#             'style': '{',
+#         },
+#         'security': {
+#             'format': ORIGIN_COLOR + '{asctime} -- ' + RED + '{levelname}'
+#                       + ORIGIN_COLOR + ' -- {module} : {message}',
+#             'style': '{',
+#         },
+#     },
+#     'filters': {
+#         'require_debug_true': {
+#             '()': 'django.utils.log.RequireDebugTrue',
+#         },
+#         'require_debug_false': {
+#             '()': 'django.utils.log.RequireDebugFalse',
+#         },
+#
+#     },
+#     'handlers': {
+#         'general': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filters': ['require_debug_false'],
+#             'filename': 'general.log',
+#             'formatter': 'standard'
+#         },
+#         'errors': {
+#             'level': 'ERROR',
+#             'class': 'logging.FileHandler',
+#             'filename': 'errors.log',
+#             'formatter': 'forerror'
+#         },
+#         'console': {
+#             'level': 'DEBUG',
+#             'filters': ['require_debug_true'],
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'standard',
+#         },
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'filters': ['require_debug_false'],
+#             'class': 'django.utils.log.AdminEmailHandler',
+#             'formatter': 'forwarning',
+#         },
+#         'security': {
+#             'level': 'INFO',
+#             'filters': ['require_debug_false'],
+#             'class': 'logging.FileHandler',
+#             'filename': 'security.log',
+#             'formatter': 'forwarning',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console', 'general'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#         'django.request': {
+#             'handlers': ['errors', 'mail_admins', 'general'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#         'django.server': {
+#             'handlers': ['errors', 'mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         'django.template': {
+#             'handlers': ['errors'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         'django.db.backends': {
+#             'handlers': ['errors'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         'django.security.*': {
+#             'handlers': ['security'],
+#             'level': 'INFO',
+#             'propagate': True,
+#         },
+#     },
+# }
